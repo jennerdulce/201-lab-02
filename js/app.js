ac1b074215883ead6a8106bd6185fd9c0cd7ff27
Jenner Dulce <jennerdulce@gmail.com>
2020-10-01 19:51:38 -0700

    added guessing game and lists

--- js/app.js
+++ js/app.js
@@ -1,99 +1,126 @@
-"use strict";
+// "use strict";
 
-// Get proper time
-var today = new Date();
-var hourNow = today.getHours();
-var greeting;
+// // Get proper time
+// var today = new Date();
+// var hourNow = today.getHours();
+// var greeting;
 
-if (hourNow > 18) {
-  greeting = 'Good Evening, ';
-} else if (hourNow > 12) {
-  greeting = 'Good Afternoon, ';
-} else if (hourNow > 0) {
-  greeting = 'Good Morning, ';
-} else {
-  greeting = 'Welcome';
-}
-
-
-// User greeting
-var customerName = prompt("What is your name?");
-document.getElementById('userGreeting').innerHTML =
-greeting + '<br>' + customerName;
+// if (hourNow > 18) {
+//   greeting = 'Good Evening, ';
+// } else if (hourNow > 12) {
+//   greeting = 'Good Afternoon, ';
+// } else if (hourNow > 0) {
+//   greeting = 'Good Morning, ';
+// } else {
+//   greeting = 'Welcome';
+// }
 
 
-// Question 1
-var userLive = prompt('Do you live in Seattle?').toLowerCase();
-
-if (userLive == 'yes' || userLive == 'y') {
-  alert('You answered: Yes');
-  console.log('Do you live in Seattle? input accepted as yes/y');
-} else if (userLive == 'no' || userLive == 'n') {
-  alert('You answered: No');
-  console.log('Do you live in Seattle? input accepted as no/n');
-} else {
-  alert('Invalid Response');
-  console.log('Do you live in Seattle? invalid response');
-}
+// // User greeting
+// var customerName = prompt("What is your name?");
+// document.getElementById('userGreeting').innerHTML =
+// greeting + '<br>' + customerName;
 
 
-// Question 2
-var userWorkout = prompt('Do you like to work out?').toLowerCase();
+// // Question 1
+// var userLive = prompt('Do you live in Seattle?').toLowerCase();
 
-if (userWorkout == 'yes' || userWorkout == 'y') {
-  alert('You answered: Yes');
-  console.log('Do you like to work out? input accepted as yes/y');
-} else if (userWorkout == 'no' || userWorkout == 'n') {
-  alert('You answered: No');
-  console.log('Do you like to work out? input accepted as no/n');
-} else {
-  alert('Invalid Response');
-  console.log('Do you like to work out? invalid response');
-}
-
-
-// Question 3
-var userRain = prompt('Do you like rain?').toLowerCase();
-
-if (userRain == 'yes' || userRain == 'y') {
-  alert('You answered: Yes');
-  console.log('Do you like rain? input accepted as yes/y');
-} else if (userRain == 'no' || userRain == 'n') {
-  alert('You answered: No');
-  console.log('Do you like rain? input accepted as no/n');
-} else {
-  alert('Invalid Response');
-  console.log('Do you like rain? invalid response');
-}
+// if (userLive == 'yes' || userLive == 'y') {
+//   alert('You answered: Yes');
+//   console.log('Do you live in Seattle? input accepted as yes/y');
+// } else if (userLive == 'no' || userLive == 'n') {
+//   alert('You answered: No');
+//   console.log('Do you live in Seattle? input accepted as no/n');
+// } else {
+//   alert('Invalid Response');
+//   console.log('Do you live in Seattle? invalid response');
+// }
 
 
-// Question 4
-var userSpicy = prompt('Do you like spicy food?').toLowerCase();
+// // Question 2
+// var userWorkout = prompt('Do you like to work out?').toLowerCase();
 
-if (userSpicy == 'yes' || userSpicy == 'y') {
-  alert('You answered: Yes');
-  console.log('Do you like spicy food? input accepted as yes/y');
-} else if (userSpicy == 'no' || userSpicy == 'n') {
-  alert('You answered: No');
-  console.log('Do you like spicy food? input accepted as no/n');
-} else {
-  alert('Invalid Response');
-  console.log('Do you like spicy food invalid response');
-}
-
-
-// Question 5
-var userPet = prompt('Do you have a pet?').toLowerCase();
-
-if (userPet == 'yes' || userPet == 'y') {
-  alert('You answered: Yes');
-  console.log('Do you have a pet? input accepted as yes/y');
-} else if (userPet == 'no' || userPet == 'n') {
-  alert('You answered: No');
-  console.log('Do you have a pet? input accepted as no/n');
-} else {
-  alert('Invalid Response');
-  console.log('Do you have a pet? invalid response');
-}
+// if (userWorkout == 'yes' || userWorkout == 'y') {
+//   alert('You answered: Yes');
+//   console.log('Do you like to work out? input accepted as yes/y');
+// } else if (userWorkout == 'no' || userWorkout == 'n') {
+//   alert('You answered: No');
+//   console.log('Do you like to work out? input accepted as no/n');
+// } else {
+//   alert('Invalid Response');
+//   console.log('Do you like to work out? invalid response');
+// }
 
 
+// // Question 3
+// var userRain = prompt('Do you like rain?').toLowerCase();
+
+// if (userRain == 'yes' || userRain == 'y') {
+//   alert('You answered: Yes');
+//   console.log('Do you like rain? input accepted as yes/y');
+// } else if (userRain == 'no' || userRain == 'n') {
+//   alert('You answered: No');
+//   console.log('Do you like rain? input accepted as no/n');
+// } else {
+//   alert('Invalid Response');
+//   console.log('Do you like rain? invalid response');
+// }
+
+
+// // Question 4
+// var userSpicy = prompt('Do you like spicy food?').toLowerCase();
+
+// if (userSpicy == 'yes' || userSpicy == 'y') {
+//   alert('You answered: Yes');
+//   console.log('Do you like spicy food? input accepted as yes/y');
+// } else if (userSpicy == 'no' || userSpicy == 'n') {
+//   alert('You answered: No');
+//   console.log('Do you like spicy food? input accepted as no/n');
+// } else {
+//   alert('Invalid Response');
+//   console.log('Do you like spicy food invalid response');
+// }
+
+
+// // Question 5
+// var userPet = prompt('Do you have a pet?').toLowerCase();
+
+// if (userPet == 'yes' || userPet == 'y') {
+//   alert('You answered: Yes');
+//   console.log('Do you have a pet? input accepted as yes/y');
+// } else if (userPet == 'no' || userPet == 'n') {
+//   alert('You answered: No');
+//   console.log('Do you have a pet? input accepted as no/n');
+// } else {
+//   alert('Invalid Response');
+//   console.log('Do you have a pet? invalid response');
+// }
+
+
+
+
+// // number guessing game
+
+// function playGuessingGame() {
+//   var i = 0;
+//   var randomNumber = Math.floor(Math.random() * 10) + 1;  // returns a random integer from 1 to 10
+  
+
+//   while (i < 4) {
+//     var userGuess = prompt('Try to guess a number between 1 and 10');
+
+//     if (userGuess == randomNumber) {
+//       alert('YOU GUESSED CORRECTLY! YOU WIN!');
+//       break;
+//     } else if (userGuess > randomNumber) {
+//       alert('You guessed too high!');
+//       i++;
+//     } else if (userGuess < randomNumber){
+//       alert('You guessed too low!');
+//       i++;
+//     } else {
+//       alert('Invalid input');
+//     }
+//   }
+//   alert('You did not guess the number. YOU LOSE!!!!!')
+// }
